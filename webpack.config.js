c18613dfdae825c57a747b4fecf8498e0110b038
Rob Levy <robert.g.levy@gmail.com>
2019-07-28 13:34:25 +0100

    Added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
@@ -16,6 +21,5 @@ module.exports = {
         }
       }
     ]
-  },
-  watch: true
+  }
 };
