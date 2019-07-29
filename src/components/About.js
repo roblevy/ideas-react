61bf90c5101bc0884ebfd23b60ec98855b90bf3b
Rob Levy <robert.g.levy@gmail.com>
2019-07-29 01:34:56 +0100

    About updated

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 const About = () => ( 
-  <main>
-    <h2>About this page</h2>
-    <p>This is a demonstration of a simple React app</p>
-  </main>
+  <section>
+    <h2 className="title">About this page</h2>
+    <p>
+      This is a demonstration of a simple React app.
+
+      It includes some of the standard setup, like Webpack and Babel, and also a CSS framework called <a href="https://bulma.io/" target="blank">Bulma</a>. 
+
+      The additional styling is done with SCSS, loaded using Webpack's <span className="is-family-monospace">sass-loader</span>.
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+
+  </section>
 )
 
 export default About;
